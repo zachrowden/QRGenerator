8644f0ad433012c4a2698d1791070e60f2a511cf
zarq6c <zarq6c@mail.missouri.edu>
2017-11-18 23:47:54 -0600

    First commit fot qr generator

---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,26 @@
+import net.glxn.qrgen.javase.QRCode
+
+class Main {
+
+        static final String dirPath    = "C:\\Users\\Zach\\Desktop\\Test\\"
+        static final String qrCodeName = "qrImage.png"
+        static final int    qrWidth    = 250
+        static final int    qrHeight   = 250
+        static final String qrContents = new Ticket.TicketBuilder()
+                                            .setOrderDate(new Date())
+                                            .setId("#1234")
+                                            .setFirstName("Zach")
+                                            .setLastName("Rowden")
+                                            .setShowDate(new Date(2017, 12, 01))
+                                            .build().toString()
+
+
+    static void main(String[] args){
+
+        File qrFile= new File("${dirPath}${qrCodeName}")
+        OutputStream output = new FileOutputStream(qrFile)
+
+        new QRCode(qrContents).withSize(qrWidth, qrHeight).writeTo(output)
+
+    }
+}
---
+++ src/main/groovy/Ticket.groovy
@@ -0,0 +1,55 @@
+class Ticket {
+
+    final String id, firstName, lastName
+    final Date   orderDate, showDate
+
+    Ticket(TicketBuilder ticketBuilder){
+        this.id        = ticketBuilder.id
+        this.firstName = ticketBuilder.firstName
+        this.lastName  = ticketBuilder.lastName
+        this.orderDate = ticketBuilder.orderDate
+        this.showDate  = ticketBuilder.showDate
+    }
+
+    @Override
+    String toString(){
+        new String("ID: \t\t\t${id}\nORDER DATE:  ${orderDate.toString()}\nNAME: \t\t${lastName}, ${firstName}\nSHOW DATE:   ${showDate.toString()}")
+    }
+
+    static class TicketBuilder{
+
+        String id, firstName, lastName
+        Date   orderDate, showDate
+
+        TicketBuilder(){}
+
+        TicketBuilder setId(String value){
+            this.id = value
+            this
+        }
+
+        TicketBuilder setFirstName(String value){
+            this.firstName = value
+            this
+        }
+
+        TicketBuilder setLastName(String value){
+            this.lastName = value
+            this
+        }
+
+        TicketBuilder setOrderDate(Date value){
+            this.orderDate = value
+            this
+        }
+
+        TicketBuilder setShowDate(Date value){
+            this.showDate = value
+            this
+        }
+
+        Ticket build(){
+            new Ticket(this)
+        }
+    }
+}
